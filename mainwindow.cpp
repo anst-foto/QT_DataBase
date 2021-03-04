654c82d741ef66efa09d024b7df49c39c5910e0d
Starinin Andrey <starininandrey@gmail.com>
2021-03-04 14:52:17 +0300

    Добавили проверку логина\пароля в БД

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,11 +24,33 @@ void MainWindow::on_button_LogIn_clicked()
 {
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("C:\\Programming\\Temp\\test_users.sqlite");
-    db.open();
 
-    if  (db.isOpen()) {
+    if  (db.open()) {
         ui->statusbar->showMessage("Подключились к БД");
+
+        QString login = ui->input_Login->text();
+        QString password = ui->input_Password->text();
+
+        QString sql = "SELECT * FROM table_account WHERE login = '" + login + "' AND password = '" + password + "'";
+        QSqlQuery query;
+        query.exec(sql);
+
+        if (query.next()) {
+            QMessageBox msgBox;
+            msgBox.setText("Вы успешно авторизовались");
+            msgBox.setIcon(QMessageBox::Information);
+            msgBox.setStandardButtons(QMessageBox::Ok);
+            msgBox.exec();
+        } else {
+            QMessageBox msgBox;
+            msgBox.setText("Вы ввели неверные данные");
+            msgBox.setIcon(QMessageBox::Warning);
+            msgBox.setStandardButtons(QMessageBox::Ok);
+            msgBox.exec();
+        }
+
     } else {
         ui->statusbar->showMessage("Не подключились к БД");
     }
+    db.close();
 }
